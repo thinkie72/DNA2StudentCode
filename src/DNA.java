4c44edb4808ba78741895796082d89b7f660892b
thinkie72 <tyler.hinkie@menloschool.org>
2024-10-14 22:58:43 -0700

    Work Log Entry 3

--- src/DNA.java
+++ src/DNA.java
@@ -24,13 +24,18 @@ public class DNA {
         lettersToVal.put('T', 3);
         lettersToVal.put('G', 4);
         int STRlength = STR.length();
+        int sequenceLength = sequence.length();
         int STRvalue = 0;
         int value = 0;
         for (int i = 0; i < STRlength; i++) {
-            STRvalue += (int) (lettersToVal.get(STR.charAt(i)) * Math.pow(10, i));
-            value += (int) (lettersToVal.get(sequence.charAt(i)) * Math.pow(10, i));
+            STRvalue += (int) (lettersToVal.get(STR.charAt(i)) * Math.pow(10, STRlength - i - 1));
+            value += (int) (lettersToVal.get(sequence.charAt(i)) * Math.pow(10, STRlength - i - 1));
         }
-        int length = sequence.length() - STRlength;
+        System.out.println(STR);
+        System.out.println(STRvalue);
+        System.out.println(sequence.substring(0, STRlength));
+        System.out.println(value);
+        int length = sequenceLength - STRlength;
         int index = 1;
         int count = 0;
         ArrayList<Integer> attempts = new ArrayList<>();
@@ -39,16 +44,18 @@ public class DNA {
                 count++;
                 value = 0;
                 for (int i = 0; i < STRlength; i++) {
-                    value += (int) (lettersToVal.get(sequence.charAt(index + i)) * Math.pow(10, i));
+                    value += (int) (lettersToVal.get(sequence.charAt(index + i - 1)) * Math.pow(10, STRlength - i - 1));
                 }
+                System.out.println(sequence.substring(index, STRlength));
+                System.out.println(value);
             } else {
                 if (count > 0) attempts.add(count);
                 count = 0;
                 value *= 10;
-                value -= (int) (lettersToVal.get(sequence.charAt(index + 1)) * Math.pow(10, STRlength + 1));
+                value -= (int) (lettersToVal.get(sequence.charAt(index)) * Math.pow(10, STRlength));
                 value += lettersToVal.get(sequence.charAt(index + STRlength));
+                index++;
             }
-            index++;
         }
 
         int maxLength = 0;
